9743d01780e2892ba8700185158a14bbabbb8371
ArindamPaulAvik <cdfrcsarie9@gmail.com>
2025-07-03 00:20:48 +0600

    cleaned

--- Backend/db.js
+++ Backend/db.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2/promise');
 // Create a connection pool
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',            // or your MySQL username
-  password: 'A132b59m!@#$PaUl', // your MySQL password
-  database: 'project_216',    // replace with your DB name
+  user: 'root',
+  password: 'A132b59m!@#$PaUl',
+  database: 'project_216',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
